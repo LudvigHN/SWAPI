ba4109434c293cca733df2fc3f36861cb27ea02f
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-10 13:46:22 +0200

    renamed params

--- script2.js
+++ script2.js
@@ -4,9 +4,9 @@ Promise.all([1, 2, 3].map(i =>
     fetch(`https://swapi.dev/api/people/?page=${i}`)
         .then(response => response.json())
 ))
-    .then(datas => {
-        datas.forEach(data => {
-            data.results.forEach(person => {
+    .then(pages => {
+        pages.forEach(page => {
+            page.results.forEach(person => {
                 const listItem = document.createElement("li")
                 listItem.textContent = person.name
                 listItem.textContent += " appers in: "
