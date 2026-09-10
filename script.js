99fb1fbc55cbd22d4e4a3a74d48e4c8cfaa73945
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-10 10:58:39 +0200

    prints first page of charecters from swapi and what movies they appear in

---
+++ script.js
@@ -0,0 +1,55 @@
+const listDOM = document.querySelector(".list")
+
+fetch("https://swapi.dev/api/people")
+    .then(response => {
+        return response.json()
+        
+        
+    })
+    .then(data =>{
+        // listDOM.innerHTML = data.results.map(person => 
+        //     `
+        //     <li>
+        //     ${person.name}
+        //     </li>
+        //     `).join("")
+            data.results.forEach(person => {
+                const listItem = document.createElement("li")
+                listItem.textContent = person.name
+                listItem.textContent+= ", appears in: "
+                Promise.all(
+                    person.films.map(film =>
+                        fetch(film)
+                        .then(response =>
+                            response.json()
+                        )
+                    ))
+                    .then(films => {
+                        
+                        
+                        listItem.textContent+= films.map(film => film.title).join(", ");
+                        
+                        
+                    })
+                
+                // person.films.forEach(film => {
+                //     fetch(film)
+                //     .then(response => {
+                //         return response.json()
+                        
+                //     })
+                //     .then(data=>{
+                //         listItem.textContent += data.title + ", "
+                        
+                //     })
+                    
+                // })
+                
+                
+                listDOM.append(listItem)
+                
+            });
+            
+
+        
+    })
